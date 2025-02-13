970a1783b4434bb7d0ecc6766de0712aa686a868
jadwro <jadwro@gmail.com>
2025-02-13 10:55:24 +0100

    added allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: [
     ['list'],
-    ['html', { open: 'never' }],
+    ['allure-playwright'],
     ['./helpers/testCaseCreator/TestCaseCreator.ts'],
   ],
   use: {
